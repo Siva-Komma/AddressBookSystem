b544654d40e68ae0ddd40bce6c507db13bd0f5bd
Siva-Komma <kommasivajyothi22@gmail.com>
2023-02-22 11:39:22 +0530

    Creating a Contact details in AddressBook

--- AddressBookSystem/ContactDetails.cs
+++ AddressBookSystem/ContactDetails.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    internal class ContactDetails
+    class ContactDetails
     {
+        public String First_Name { get; set; }
+        public String Last_Name { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String State { get; set; }
+        public long Zip_Code { get; set; }
+        public long PhoneNumber { get; set; }
+        public String Email { get; set; }
     }
 }
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace AddressBookSystem
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
